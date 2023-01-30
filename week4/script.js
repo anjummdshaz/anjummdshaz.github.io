31606c80129db5b707ed726f384e55c9f426234d
anjummdshaz <112770250+anjummdshaz@users.noreply.github.com>
2023-01-30 14:43:37 +0530

    updated

--- week4/script.js
+++ week4/script.js
@@ -1,14 +1,14 @@
 var names=new Array();
 names[0]="Yaakov";
-names[1]="John";
-names[2]="Jen";
-names[3]="jason";
-names[4]="paul";
-names[5]="frank";
-names[6]="larry";
-names[7]="paula";
-names[8]="laura";
-names[9]="jim";
+names[1]="Johhny";
+names[2]="Jenifr";
+names[3]="stepnie";
+names[4]="hagen";
+names[5]="franksten";
+names[6]="paula";
+names[7]="pauly";
+names[8]="jasen";
+names[9]="jimmy";
 
 
 for (var i = 0; i < names.length; i++) {
